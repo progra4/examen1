908df726ff5c3ee28b39a31590c5df2f33579bca
Luis Borjas <luisfborjas@gmail.com>
2012-05-06 02:17:48 -0600

    vil copia

---
+++ app.rb
@@ -0,0 +1,22 @@
+require 'rack'
+require './controllers'
+
+class WebApp
+  
+  include Controllers
+
+  def call(env)
+
+    request = Rack::Request.new(env)
+    controller = QuotesController.new(request)
+    
+    status, body = controller.dispatch
+
+    [
+     status,
+     #los valores de los headers *deben* ser String
+     {'Content-Type' => 'text/plain', 'Content-Length' => body.size.to_s},
+     [body]
+    ]
+  end
+end
---
+++ controllers.rb
@@ -0,0 +1,90 @@
+require './models'
+module Controllers
+  class NotFoundException < Exception; end
+
+  class Controller
+    
+    attr_accessor :request
+
+    def initialize(request)
+      @request = request
+    end
+
+    def route
+      collection_pattern = /\/#{resource_name}$/
+      member_pattern     = /\/#{resource_name}\/([a-z0-9\-]+)/
+
+
+      #collection actions
+      if request.path =~ collection_pattern
+        if request.get?
+          return index
+        elsif request.post?
+          return create
+        else
+          return [405, ""]
+        end
+      end
+      
+      #member actions
+      if request.path =~ member_pattern
+        request.params["id"] = request.path.match(member_pattern)[1]
+        if request.get?
+          # show <=> read
+          return show
+        elsif request.put?
+          return update
+        elsif request.delete?
+          return destroy
+        else
+          return [405, ""]
+        end
+      end
+
+      #a not implemented path was requested
+      return [501, ""]
+    end
+
+
+    def dispatch
+      begin
+        route 
+      rescue NotFoundException
+        [404, ""]
+      rescue 
+        [500, ""]
+      end
+    end
+  end
+
+
+  class QuotesController < Controller
+    include Models
+
+    def resource_name
+      "quotes"
+    end
+
+    def index
+      [200, Quote.all.map(&:as_text).join("\n")]
+    end
+
+    def show
+      quote = Quote.find(request.params["id"])
+      if quote
+        [200, quote.as_text]
+      else
+        raise NotFoundException
+      end
+    end
+
+    def create
+      quote = Quote.create(
+        content: request.params["content"],
+        author:  request.params["author"]
+      )
+
+      [201, quote.as_text]
+    end
+  end
+end
